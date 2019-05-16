a5487ee9a4e7d282543e7e8d3b86d87bce5fe5a8
Michał Grzelak <grzelakm@outlook.com>
2019-05-16 16:32:51 +0200

    add @Before init method and setup for AtmMachine class

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -3,13 +3,28 @@ package edu.iis.mto.testreactor.exc3;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class AtmMachineTest {
+
+    private CardProviderService cardProviderService;
+    private BankService bankService;
+    private MoneyDepot moneyDepot;
+    private AtmMachine atmMachine;
+
+    @Before
+    public void init() {
+        cardProviderService = Mockito.mock(CardProviderService.class);
+        bankService = Mockito.mock(BankService.class);
+        moneyDepot = Mockito.mock(MoneyDepot.class);
+
+        atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
-
 }
